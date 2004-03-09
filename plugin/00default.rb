982383902a38845a58bed66e8a1b50eb29296836
masao <tmasao@acm.org>
2004-03-09 00:32:42 +0000

    <link rel="chapter" ...> 対応（latest のみ）

--- plugin/00default.rb
+++ plugin/00default.rb
@@ -158,6 +158,13 @@ def index_page_tag
 
    result << %Q[<link rel="last" title="#{navi_latest}" href="#{@conf.index}">\n\t]
 
+   if @mode == "latest" then
+      month_list = @years.keys.collect{|y| @years[y]}.flatten.sort
+      month_list.each do |m|
+         result << %Q[<link rel="chapter" title="#{m[0,4]}-#{m[4,2]}" href="#{@conf.index}#{anchor m}">\n\t]
+      end
+   end
+
    result.strip
 end
 
