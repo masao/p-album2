f380cb2f16f6d0e846ccd0cb2e34882afabfb0b4
masao <tmasao@acm.org>
2004-03-10 15:57:27 +0000

    Upload テストを追加

--- test.rb
+++ test.rb
@@ -1,11 +1,16 @@
 #!/usr/local/bin/ruby
 # $Id$
 
-require 'test/unit/testsuite'
-require 'test/unit/testcase'
+require 'test/unit'
 require 'ftools'
 
 require 'p-album'
+
+class CGI
+   # For local test: cf.[ruby-list:34638]
+   remove_const(:EOL)
+   EOL = "\n"
+end
 
 def rm_rf( path )
    if FileTest::directory? path
@@ -18,25 +23,43 @@ def rm_rf( path )
    end
 end
 
+def tmpname
+   File::join( "/tmp", self.class.to_s.downcase + "_" + $$.to_s )
+end
+
+def setup_tmpdir
+   tmpdir = tmpname
+   Dir::mkdir( tmpdir )
+   images_dir = File::join( tmpdir, "images/" )
+   thumbs_dir = File::join( tmpdir, "thumbs/" )
+   Dir::mkdir( images_dir )
+   Dir::mkdir( thumbs_dir )
+   tmpdir
+end
+
+def setup_album_conf( dir )
+   open( "p-album.conf", "w" ) do |f|
+      f.print <<-EOF
+@data_path = '#{dir}/'
+@images_dir = '#{dir}/images/'
+@thumbs_dir = '#{dir}/thumbs/'
+@convert = 'convert'
+@thumbnail_opts = [ "-geometry", "96x96>", "+profile", "*" ]
+      EOF
+   end
+end
+
 class TestPhotoFile < Test::Unit::TestCase
    include PhotoAlbum
    def setup
-      @tmpdir = File::join( "/tmp", self.class.to_s.downcase + "_" + $$.to_s )
-      Dir::mkdir( @tmpdir )
-      images_dir = File::join( @tmpdir, "images/" )
-      thumbs_dir = File::join( @tmpdir, "thumbs/" )
-      Dir::mkdir( images_dir )
-      Dir::mkdir( thumbs_dir )
-
-      c = Struct::new( "Config", :images_dir, :thumbs_dir, :perm, :convert, :thumbnail_opts )
-      @conf = c::new( images_dir, thumbs_dir, 0600, "convert",
-                      [ "-geometry", "96x96>", "+profile", "*" ] )
-
+      @tmpdir = setup_tmpdir
+      setup_album_conf( @tmpdir )
+      @conf = Config::new
       @photo = PhotoFile::load_file( "DSC05003.JPG", @conf )
    end
-
    def teardown
       rm_rf @tmpdir
+      rm_rf "p-album.conf"
    end
 
    def test_load_file
@@ -65,16 +88,40 @@ class TestPhotoFile < Test::Unit::TestCase
    end
 end
 
-class TestPhotoAlbumSearch < Test::Unit::TestCase
+class TestAlbumUpload < Test::Unit::TestCase
    include PhotoAlbum
-end
+   def setup
+      @tmpdir = setup_tmpdir
+      setup_album_conf( @tmpdir )
+      @conf = Config::new
 
+      tmpfile = @tmpdir + "/http.dat"
+      open( tmpfile, "w" ) do |f|
+         f.print <<-EOF
+--boundary
+Content-Disposition: form-data; name="file1"; filename="file1"
+Content-Type: image/jpeg
 
-if $0 == __FILE__
-   suite = Test::Unit::TestSuite.new('PhotoAlbum')
-   ObjectSpace.each_object(Class) do |klass|
-      suite << klass.suite if (Test::Unit::TestCase > klass)
+         EOF
+         f.print open('DSC05003.JPG'){|image| image.read;}
+         f.print "\n--boundary--\n"
+      end
+
+      ENV['REQUEST_METHOD'] = 'POST'
+      ENV['CONTENT_LENGTH'] = FileTest::size?( tmpfile ).to_s
+      ENV['CONTENT_TYPE'] = 'multipart/form-data; boundary=boundary'
+      $stdin = open( tmpfile )
+      @album = AlbumUpload::new( CGI::new, "update.rhtml", @conf )
    end
-   require 'test/unit/ui/console/testrunner'
-   Test::Unit::UI::Console::TestRunner.run(suite).passed?
+   def teardown
+      rm_rf @tmpdir
+      rm_rf "p-album.conf"
+   end
+
+   def test_new
+      assert( FileTest::exist?( File::join(@conf.images_dir,"20040301t091255.jpg")) )
+      assert( FileTest::exist?( File::join(@conf.thumbs_dir,"20040301t091255.jpg")) )
+      im = ImageSize::new( open( File::join(@conf.thumbs_dir,"20040301t091255.jpg") ) )
+      assert_equal( 96, [ im.get_width, im.get_height ].max )
+   end
 end
