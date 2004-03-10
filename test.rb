5a7edd66640de2f7fbc29de699abd535cf850192
masao <tmasao@acm.org>
2004-03-10 09:25:06 +0000

    ちょっと修正

--- test.rb
+++ test.rb
@@ -31,6 +31,8 @@ class TestPhotoFile < Test::Unit::TestCase
       c = Struct::new( "Config", :images_dir, :thumbs_dir, :perm, :convert, :thumbnail_opts )
       @conf = c::new( images_dir, thumbs_dir, 0600, "convert",
                       [ "-geometry", "96x96>", "+profile", "*" ] )
+
+      @photo = PhotoFile::load_file( "DSC05003.JPG", @conf )
    end
 
    def teardown
@@ -38,9 +40,10 @@ class TestPhotoFile < Test::Unit::TestCase
    end
 
    def test_load_file
-      photo = PhotoFile::load_file( "DSC05003.JPG", @conf )
-      assert_instance_of( PhotoFile, photo )
-      assert_equal( "20040301t091255", photo.name )
+      assert_instance_of( PhotoFile, @photo )
+      assert_equal( "20040301t091255", @photo.name )
+
+      assert_nil( PhotoFile::load_file( "DSC05003.JPG", @conf ) )
 
       assert( FileTest::exist?( File::join(@conf.images_dir,"20040301t091255.jpg")) )
       assert( FileTest::exist?( File::join(@conf.thumbs_dir,"20040301t091255.jpg")) )
@@ -50,13 +53,22 @@ class TestPhotoFile < Test::Unit::TestCase
    end
 
    def test_do_convert
-      photo = PhotoFile::load_file( "DSC05003.JPG", @conf )
-      photo.do_convert( 90, nil )
+      @photo.do_convert( 90, nil )
       im = ImageSize::new( open( File::join(@conf.images_dir,"20040301t091255.jpg") ) )
       assert_equal( 640, im.get_height )
       assert_equal( 480, im.get_width )
+
+      assert( FileTest::exist?( File::join(@conf.images_dir,"20040301t091255.jpg.orig")) )
+      im = ImageSize::new( open( File::join(@conf.images_dir,"20040301t091255.jpg.orig") ) )
+      assert_equal( 480, im.get_height )
+      assert_equal( 640, im.get_width )
    end
 end
+
+class TestPhotoAlbumSearch < Test::Unit::TestCase
+   include PhotoAlbum
+end
+
 
 if $0 == __FILE__
    suite = Test::Unit::TestSuite.new('PhotoAlbum')
