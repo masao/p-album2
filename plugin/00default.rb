bdaef77a21a2cf12f256e7ea42686683a4dc9627
masao <tmasao@acm.org>
2006-05-05 00:58:20 +0000

    use navi_{prev,next}_photo for navi's image alt/titles.

--- plugin/00default.rb
+++ plugin/00default.rb
@@ -5,10 +5,10 @@ def navi
    result = %Q[<div class="adminmenu">\n]
    result << %Q[<span class="adminmenu"><a href="#{@conf.index_page}">¥È¥Ã¥×</a></span>\n] unless @conf.index_page.empty?
    result << %Q[<span class="adminmenu"><a href="#{@conf.index}#{anchor @prev_month}">&laquo;#{navi_prev_month}</a></span>\n] if @prev_month
-   result << %Q[<span class="adminmenu"><a href="#{@conf.index}#{anchor @prev_photo.name}"><img src="#{@prev_photo.thumbnail}" alt="#{@prev_photo.title}" title="#{@prev_photo.title}" #{html_imgsize(@prev_photo.thumbnail)}>&laquo;#{navi_prev_photo}</a></span>\n] if @prev_photo
+   result << %Q[<span class="adminmenu"><a href="#{@conf.index}#{anchor @prev_photo.name}"><img src="#{@prev_photo.thumbnail}" alt="#{navi_prev_photo}" title="#{navi_prev_photo}" #{html_imgsize(@prev_photo.thumbnail)}>&laquo;#{navi_prev_photo}</a></span>\n] if @prev_photo
    result << %Q[<span class="adminmenu"><a href="#{@conf.index}">#{navi_latest}</a></span>\n] unless @mode == 'latest'
    result << %Q[<span class="adminmenu"><a href="#{@conf.index}#{anchor @next_month}">#{navi_next_month}&raquo;</a></span>\n] if @next_month
-   result << %Q[<span class="adminmenu"><a href="#{@conf.index}#{anchor @next_photo.name}">#{navi_next_photo}&raquo;<img src="#{@next_photo.thumbnail}" alt="#{@next_photo.title}" title="#{@next_photo.title}" #{html_imgsize(@next_photo.thumbnail)}></a></span>\n] if @next_photo
+   result << %Q[<span class="adminmenu"><a href="#{@conf.index}#{anchor @next_photo.name}">#{navi_next_photo}&raquo;<img src="#{@next_photo.thumbnail}" alt="#{navi_next_photo}" title="#{navi_next_photo}" #{html_imgsize(@next_photo.thumbnail)}></a></span>\n] if @next_photo
    result << %Q[<span class="adminmenu"><a href="#{@conf.index}#{anchor @photo.name}">#{navi_back}</a></span>\n] if @mode =~ /^photo(edit|convert|original|save)$/
    result << %Q[<span class="adminmenu"><a href="#{@conf.update}?photo=#{@photo.name}">#{navi_edit}</a></span>\n] if @mode == 'photo'
    menu_proc.each {|i| result << %Q[<span class="adminmenu">#{i}</span>\n]}
